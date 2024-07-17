ebe5c90dd4fe8cbfa72202313376fd4a17245e84
Vitaliy Yarash <vit.iarosch@yandex.by>
2024-07-17 13:48:39 +0300

    feat: add share btn

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,19 +1,30 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         val btnBack = findViewById<ViewGroup>(R.id.btn_back)
-
         btnBack.setOnClickListener {
             this.finish()
         }
+
+        val btnShare = findViewById<ViewGroup>(R.id.btn_share)
+        btnShare.setOnClickListener {
+            val message = "https://practicum.yandex.ru/android-developer/?from=catalog"
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.setType("text/plain")
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(intent)
+        }
     }
+
 }
