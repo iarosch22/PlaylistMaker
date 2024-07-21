ad5a740c4b949d959b74ffa8306dbb2137ca3d3e
Vitaliy Yarash <vit.iarosch@yandex.by>
2024-07-21 13:25:20 +0300

    feat: add search page layout

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,40 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val btnBack = findViewById<ViewGroup>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            this.finish()
+        }
+
+        val container = findViewById<LinearLayout>(R.id.containerInput)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearBtn = findViewById<ImageView>(R.id.clearIcon)
+
+        clearBtn.setOnClickListener {
+            inputEditText.setText("")
+        }
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+
+    }
+
 }
